22b729ff4a20c0f8750b638e5407a592a414179f
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2024-12-25 21:14:03 +0500

    Add files via upload

--- src/components/GreenHeader.tsx
+++ src/components/GreenHeader.tsx
@@ -16,46 +16,46 @@ export default function Greenheader() {
       {/* Top Bar */}
       <div className="hidden md:flex flex-col h-[58px] md:flex-row items-center justify-between bg-[#23856D] px-4 py-2 text-sm">
         {/* Contact Info Section */}
-        <div className="flex items-center space-x-4 text-white font-bold">
+        <div className="flex items-center space-x-3 text-white font-bold">
           {/* Phone Number */}
-          <FaPhoneAlt className="text-2xl" />
-          <a href="#" className="hover:text-cyan-400 text-xl">
+          <FaPhoneAlt className="text-sm" />
+          <a href="#" className="hover:text-green-300 text-sm">
             (225) 555-0118
           </a>
           {/* Email Address */}
-          <MdOutlineMailOutline className="text-2xl" />
-          <a href="#" className="hover:text-cyan-400 text-xl">
+          <MdOutlineMailOutline className="text-sm" />
+          <a href="#" className="hover:text-green-300 text-sm">
             michelle.rivera@example.com
           </a>
         </div>
 
         {/* Promotional Message */}
-        <div className="flex items-center space-x-2 text-white font-bold text-xl">
-          <a href="#" className="font-bold hover:text-cyan-400">
+        <div className="flex items-center space-x-2 text-white font-bold text-sm">
+          <a href="#" className="font-bold hover:text-green-300">
             Follow Us and get a chance to win 80% off
           </a>
         </div>
 
         {/* Social Media Links */}
         <div className="flex items-center space-x-3 text-white">
-          <span className="hover:text-cyan-400 text-xl font-bold">
+          <span className="hover:text-green-300 text-sm font-bold">
             Follow Us:
           </span>
           {/* Facebook */}
           <a href="#">
-            <FaFacebook className="text-lg hover:text-cyan-400" />
+            <FaFacebook className="text-sm hover:text-green-300" />
           </a>
           {/* YouTube */}
           <a href="#">
-            <FaYoutube className="text-lg hover:text-cyan-400" />
+            <FaYoutube className="text-sm hover:text-green-300" />
           </a>
           {/* Instagram */}
           <a href="#">
-            <FaInstagram className="text-lg hover:text-cyan-400" />
+            <FaInstagram className="text-sm hover:text-green-300" />
           </a>
           {/* Twitter */}
           <a href="#">
-            <FaTwitter className="text-lg hover:text-cyan-400" />
+            <FaTwitter className="text-sm hover:text-green-300" />
           </a>
         </div>
       </div>
